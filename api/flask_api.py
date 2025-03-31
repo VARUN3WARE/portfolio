b9d7979c12c9464100c9339557d151b0ca88f916
Varun Rao <106867498+VARUN3WARE@users.noreply.github.com>
2025-03-31 17:02:07 +0530

    Update flask_api.py
    debug=false

--- api/flask_api.py
+++ api/flask_api.py
@@ -96,4 +96,4 @@ def handler(request):
 
 # If running locally (optional), this line is used for local testing
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=False, host='0.0.0.0', port=5000)
